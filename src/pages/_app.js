43c54b65975c3fe22137567dc615e3b615ad3309
OrlandoQuintana <opinoquintana@yahoo.com>
2023-05-11 23:05:28 -0500

    title + description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css'
 import React from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Clumsy View</title>
+                <meta name="description" content="Clumsy NFT Wallet Viewer. Search any Cardano Address, ADA Handle, or Stake Key to view your Clumsy NFTs!" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
+
